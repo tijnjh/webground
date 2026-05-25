a6bdb1b01c09057efa8ef0283f80741506694286
tijn <tijn@tijn.dev>
2026-05-25 23:34:25 +0200

    chore: replace svelte-adapter-bun with @sveltejs/adapter-static in package.json and svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from 'svelte-adapter-bun';
+import adapter from '@sveltejs/adapter-static';
 
 export default {
 	preprocess: vitePreprocess(),
