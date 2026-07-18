5e5f3b68da40abd5a6f7c744e961914842ac205b
tijn <tijn@tijn.dev>
2026-07-18 17:54:14 +0200

    feat(editor): refactor editor component to support multiple languages with LangEditor

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,3 +1,4 @@
+import type { LangUnion } from '#lib/types'
 import Monaco from '@monaco-editor/react'
 import { useAtomValue } from 'jotai'
 import { useTheme } from 'next-themes'
@@ -8,11 +9,7 @@ import { MenuBar } from './menu-bar'
 
 export function Editor() {
   const selectedTab = useAtomValue(selectedTabAtom)
-  const isShared = useIsShared()
   const { code, setCode } = useCodeStore()
-  const { resolvedTheme } = useTheme()
-
-  const lang = selectedTab === 'js' ? 'javascript' : selectedTab
 
   return (
     <div
@@ -21,29 +18,62 @@ export function Editor() {
       <MenuBar />
 
       <div className="relative isolate h-full *:absolute *:inset-0 *:h-full *:transition-[filter] *:duration-500">
-        <Monaco
-          className="absolute inset-0"
-          theme={resolvedTheme === 'dark' ? 'vs-dark' : 'vs'}
-          onChange={value => setCode({ ...code, [selectedTab]: value })}
-          value={code[selectedTab]}
-          language={lang}
-          options={{
-            readOnly: isShared,
-            value: code[selectedTab],
-            language: lang,
-            overviewRulerLanes: 0,
-            overviewRulerBorder: false,
-            automaticLayout: true,
-            cursorBlinking: 'smooth',
-            smoothScrolling: true,
-            fontSize: 13,
-            minimap: { enabled: false },
-            tabSize: 2,
-            fontFamily: 'MonoLisaCode',
-            fontLigatures: true,
-          }}
-        />
+        {(['html', 'css', 'js'] as const).map(lang => (
+          <div
+            key={lang}
+            aria-hidden={selectedTab !== lang}
+            className={selectedTab === lang ? 'visible z-10' : 'invisible pointer-events-none'}
+          >
+            <LangEditor
+              lang={lang}
+              setCodeByLang={value => setCode(current => ({
+                ...current,
+                [lang]: value,
+              }))}
+              codeByLang={code[lang]}
+            />
+          </div>
+        ))}
       </div>
     </div>
+  )
+}
+
+export function LangEditor({
+  lang,
+  codeByLang,
+  setCodeByLang,
+}: {
+  lang: LangUnion
+  codeByLang: string
+  setCodeByLang: (code: string) => void
+}) {
+  const { resolvedTheme } = useTheme()
+  const isShared = useIsShared()
+
+  const language = lang === 'js' ? 'javascript' : lang
+
+  return (
+    <Monaco
+      className="absolute inset-0"
+      theme={resolvedTheme === 'dark' ? 'vs-dark' : 'vs'}
+      path={`file:///${lang}`}
+      onChange={value => value !== undefined && setCodeByLang(value)}
+      value={codeByLang}
+      language={language}
+      options={{
+        readOnly: isShared,
+        overviewRulerLanes: 0,
+        overviewRulerBorder: false,
+        automaticLayout: true,
+        cursorBlinking: 'smooth',
+        smoothScrolling: true,
+        fontSize: 13,
+        minimap: { enabled: false },
+        tabSize: 2,
+        fontFamily: 'MonoLisaCode',
+        fontLigatures: true,
+      }}
+    />
   )
 }
